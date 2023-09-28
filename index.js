4f428856c6ed5219af01b22db7c4c73c5b3a25d3
Bukunmi-sketch <Olarindebukunmi@gmail.com>
2023-09-28 22:05:08 +0100

    still learning

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 var express = require("express");
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+
 var app = express();
 
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
-app.get("/", function(req, res){
-    res.send("Hello world");
+// var things = require("./things.js");
+// app.use("/things", things);
+
+// app.use(function(req,res, next){
+//     console.log("start");
+//     next();
+// });
+
+// app.get("/", function(req, res, next){
+//     res.send("Middle");
+//     next();
+// })
+
+// app.use("/", function(req, res){
+//     console.log("end");
+// })
+
+app.get("*", function(req,res){
+    res.send("invalid url");
 })
+
+
 
 app.listen(3000);
